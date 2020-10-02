5c618e5100289d765503c751fb85cd596b0489ae
Adam Kaplan <adam.kaplan@thoughtworks.com>
2020-10-02 11:15:11 -0700

    [AK] adds styling changes to DimensionsEditor

--- src/components/dimensionsEditor/DimensionsEditor.jsx
+++ src/components/dimensionsEditor/DimensionsEditor.jsx
@@ -5,10 +5,15 @@ const dimensionsEditor = props => {
     const dimensions = props.dimensions;
 
     return (
-        <div className="DimensionsEditor">
+        <div className="DimensionsEditor mb-3">
             <h2>Dimensions</h2>
             {
-                dimensions.map(e => <div className="DimensionsEditorItem" key={e.index}><p>{e.name}</p><button data-dimensionindex={e.index} onClick={props.onRemoveDimension}>Remove</button></div>)
+                dimensions.map(e => (<div className="DimensionsEditorItem" key={e.index}>
+                    <p>{e.name}</p>
+                    <button type="button" className="btn btn-danger" data-dimensionindex={e.index} onClick={props.onRemoveDimension}>
+                        Remove
+                    </button>
+                </div>))
             }
         </div >
     );
